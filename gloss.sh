361b0e62a7733881e06f8453aee76220e0f7ba95
David Bain <pigeonflight@gmail.com>
2014-10-20 10:55:36 -0500

    Create gloss.sh

---
+++ gloss.sh
@@ -0,0 +1,21 @@
+curl -L https://raw.githubusercontent.com/GlossProject/glossproject.github.io/master/heroku.cfg > heroku.cfg
+mkdir pypi_local
+cd pypi_local
+curl -OL https://github.com/GlossProject/gloss.theme/releases/download/0.5.3/gloss.theme-0.5.3.zip # gloss.theme-0.5.3.zip
+curl -OL https://github.com/GlossProject/gloss.policy/releases/download/0.5.1/gloss.policy-0.5.1.zip # gloss.policy-0.5.zip
+cd ..
+git init
+git add .
+git commit -am 'initial setup of gloss'
+heroku create --buildpack git://github.com/pigeonflight/heroku-buildpack-plone
+echo "-----------------------------------------------------------------"
+echo "-                                                               -"
+echo "-   Look at you all Glossy :)! Next do the following:           -"
+echo "-                                                               -"
+echo "-   1. set a password in the heroku.cfg file                    -"
+echo "-   2. run the command:                                         -"
+echo "-           git commit -am 'updated the password'               -"
+echo "-   3. push your changes to heroku with this command            -"
+echo "-           git push heroku master                              -"
+echo "-                                                               -"
+echo "-----------------------------------------------------------------"
